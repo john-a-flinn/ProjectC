9ee88c3c082629edaa18796d2858cd153aae67c4
john-a-flinn <flinnj@hawaii.edu>
2025-01-04 11:25:11 -1000

    Delete header

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { prisma } from '@/lib/prisma';
 import ProductTable from '@/components/ProductTable'; // Adjust the path as needed
-import Header from '@/components/Header';
+
 export default async function Page() {
   // Fetch data from the joblist table
   const joblist = await prisma.joblist.findMany();
@@ -11,7 +11,6 @@ export default async function Page() {
       <section>
         <h2>Job List</h2>
         {/* Pass the fetched joblist data to ProductTable */}
-        <Header/>
         <ProductTable joblist={joblist} />
         
       </section>
